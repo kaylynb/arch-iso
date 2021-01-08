30a2154070e342526e30b9f14c5483fc4885be83
Kaylyn Bogle <kaylyn@kaylyn.ink>
2021-01-08 15:55:26 -0800

    Remove async code
    Update dockerfile base image
    Update machine configs

--- generate-iso/index.js
+++ generate-iso/index.js
@@ -2,44 +2,31 @@
 
 const nunjucks = require('nunjucks')
 const shelljs = require('shelljs')
-const co = require('co')
 const path = require('path')
-const fs = require('mz/fs')
-const mkdirp = require('thenify')(require('mkdirp'))
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const child_process = require('child_process')
+const os = require('os')
 
 const config = require(`./${process.argv[2]}`)
-
-const child_process = require('child_process')
-
-const exec = (cmd, opts) => {
-	return new Promise((resolve, reject) => {
-		const emitter = child_process.exec(cmd, opts, (err, stdout) => {
-			if (err) {
-				reject()
-			} else {
-				resolve(stdout.trim())
-			}
-		})
-
-		emitter.stdout.pipe(process.stdout)
-	})
-}
 
 nunjucks.configure({
 	autoescape: false,
 	throwOnUndefined: true
 })
 
+const exec = cmd => child_process.execSync(cmd, { stdio: 'inherit' })
+
 let buildDirectory = undefined
-const setBuildDirectory = function * () {
-	buildDirectory = yield exec('mktemp -d /tmp/generate-isoXXXXXX', { silent: true })
+const setBuildDirectory = () => {
+	buildDirectory = fs.mkdtempSync(path.join(os.tmpdir(), 'generate-iso'))
 	console.log(`Build directory created [${buildDirectory}]`)
 }
 
-const renderTpl = function * (filePath) {
-	yield mkdirp(path.resolve(buildDirectory, path.dirname(filePath)))
+const renderTpl = filePath => {
+	mkdirp.sync(path.resolve(buildDirectory, path.dirname(filePath)))
 
-	yield fs.writeFile(
+	fs.writeFileSync(
 		path.resolve(buildDirectory, filePath),
 		nunjucks.render(
 			path.resolve(__dirname, path.join('tpl', filePath)),
@@ -48,28 +35,25 @@ const renderTpl = function * (filePath) {
 	)
 }
 
-co(function * () {
-	try {
-		yield setBuildDirectory()
-		shelljs.cp('-R', path.resolve(__dirname, 'isofiles/'), path.join(buildDirectory, 'isofiles/'))
-		yield renderTpl('build-iso.sh')
-		yield renderTpl('isofiles/root/install.sh')
-		yield renderTpl('isofiles/root/chroot-install.sh')
-		yield renderTpl('Dockerfile')
-		yield renderTpl('isofiles/etc/vconsole.conf')
+try {
+	setBuildDirectory()
+	shelljs.cp('-R', path.resolve(__dirname, 'isofiles/'), path.join(buildDirectory, 'isofiles/'))
+	renderTpl('build-iso.sh')
+	renderTpl('isofiles/root/install.sh')
+	renderTpl('isofiles/root/chroot-install.sh')
+	renderTpl('Dockerfile')
+	renderTpl('isofiles/etc/vconsole.conf')
 
-		const buildName = `archlinux-isobuild-${Date.now()}`
+	const buildName = `archlinux-isobuild-${Date.now()}`
 
-		yield exec(`docker build --no-cache -t ${buildName} ${buildDirectory}`)
-		yield exec(`docker run --privileged --rm -v ${path.resolve(__dirname, '../iso/')}:/archiso/out/ ${buildName} /bin/bash usr/sbin/build-iso.sh`)
-		yield exec(`docker rmi ${buildName}`)
-	} finally {
-		if (buildDirectory) {
-			console.log(`Removing build directory [${buildDirectory}]`)
-			shelljs.rm('-rf', buildDirectory)
-		}
+	exec(`docker build --no-cache -t ${buildName} ${buildDirectory}`)
+	exec(`docker run --privileged --rm -v ${path.resolve(__dirname, '../iso/')}:/archiso/out/ ${buildName} /bin/bash usr/sbin/build-iso.sh`)
+	exec(`docker rmi ${buildName}`)
+} catch (err) {
+	console.error(err)
+} finally {
+	if (buildDirectory) {
+		console.log(`Removing build directory [${buildDirectory}]`)
+		shelljs.rm('-rf', buildDirectory)
 	}
-}).catch(err => {
-	console.log('ERROR')
-	console.log(err)
-})
+}
--- generate-iso/tpl/build-iso.sh
+++ generate-iso/tpl/build-iso.sh
@@ -8,22 +8,21 @@ cp -r /usr/share/archiso/configs/releng/* /archiso
 cp -r /isofiles/* /archiso/airootfs/
 
 chmod +x /archiso/airootfs/root/install.sh
-rm /archiso/airootfs/root/install.txt
+
+mkdir -p airootfs/etc/pacman.d
+cp /etc/pacman.d/mirrorlist airootfs/etc/pacman.d/
 
 cd /archiso
+sed -i 's/broadcom-wl/#broadcom-wl/' packages.x86_64
 echo "{{ packages.iso.packages | join("\n") }}" >> packages.x86_64
 
-sed -i 's@curl -o ${work_dir}/x86_64/airootfs/etc/pacman.d/mirrorlist@#@' build.sh
-sed -i 's/lynx -dump -nolist/#/' build.sh
-
 {% if sshd %}
-echo "sed -i 's/#\(PermitEmptyPasswords \).\+/\1yes/' /etc/ssh/sshd_config" >> airootfs/root/customize_airootfs.sh
-echo 'systemctl enable sshd' >> airootfs/root/customize_airootfs.sh
-sed -i '/INCLUDE/d' syslinux/archiso_sys.cfg
-sed -i '1iDEFAULT select\nPROMPT 0\nTIMEOUT 50\nDEFAULT arch64' syslinux/archiso_sys.cfg
+sed -i 's/#\(PermitEmptyPasswords \).\+/\1yes/' airootfs/etc/ssh/sshd_config
+mkdir -p airootfs/etc/systemd/system/multi-user.target.wants
+ln -s /usr/lib/systemd/system/sshd.service airootfs/etc/systemd/system/multi-user.target.wants/
 {% endif %}
 
 echo 'systemctl enable systemd-networkd' >> airootfs/root/customize_airootfs.sh
 echo 'systemctl enable systemd-resolved' >> airootfs/root/customize_airootfs.sh
 
-./build.sh -v -N archlinux-{{ hostname }}
+mkarchiso -v /archiso
